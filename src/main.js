0f815fc24ecc2c35ff1261f0e20174cd3e55dbbf
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-10-03 23:42:38 +0300

    refactor: replaced plain css sakura imports with scss

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import "normalize.css/normalize.css";
-import "./assets/fonts/styles.css";
-import "./assets/css/sakura.css";
-import "./assets/css/sakura-dark.css";
+import 'normalize.css/normalize.css';
+import '~assets/fonts/styles.css';
+import '~assets/styles/sakura.scss';
 
-import { ViteSSG } from "vite-ssg";
-import App from "./App.vue";
-import routes from "~pages";
+import { ViteSSG } from 'vite-ssg';
+import App from './App.vue';
+import routes from '~pages';
 
 export const createApp = ViteSSG(App, { routes });
