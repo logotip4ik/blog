ffc1cf252356f7b7b18ac28df6544599ffd11d7c
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-10-03 23:44:18 +0300

    feat: filtering posts to render

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,13 @@
 <script setup>
-import pages from "~pages";
+import pages from '~pages';
+
+const posts = computed(() => pages.filter((page) => page.path.includes('/p')));
 </script>
 
 <template>
   <div>
     <ul>
-      <li v-for="page in pages" :key="page.path">
+      <li v-for="page in posts" :key="page.path">
         <RouterLink :to="page.path">
           {{ page.name }}
         </RouterLink>
