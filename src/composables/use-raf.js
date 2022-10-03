7f1d9dbf3d95bfb0d9107fe2fa238df4b6a5f532
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-10-03 23:46:47 +0300

    feat: hook for added function into request animation frame

---
+++ src/composables/use-raf.js
@@ -0,0 +1,28 @@
+import { onMounted } from "vue";
+
+const callbacks = new Map();
+
+let animationFrame;
+
+export default function () {
+  if (!animationFrame) onMounted(createAnimationFrame);
+
+  return {
+    add: (value) => callbacks.set(value, value),
+    remove: (value) => callbacks.delete(value),
+  };
+}
+
+function createAnimationFrame() {
+  const loop = (time) => {
+    callbacks.forEach((value) => typeof value === "function" && value(time));
+
+    requestAnimationFrame(loop);
+  };
+
+  animationFrame = requestAnimationFrame(loop);
+
+  window.addEventListener("beforeunload", () =>
+    cancelAnimationFrame(animationFrame)
+  );
+}
