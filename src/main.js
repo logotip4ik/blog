4818a31225798d2e2ffb22b27b960b3303a6df2d
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-10-04 23:06:21 +0300

    refactor: new scroll behaivor for vue router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import 'normalize.css/normalize.css';
+import '@fontsource/ibm-plex-sans';
 import '~assets/fonts/styles.css';
 import '~assets/styles/sakura.scss';
+import '~assets/styles/code.scss';
 
 import { ViteSSG } from 'vite-ssg';
 import App from './App.vue';
 import routes from '~pages';
 
-export const createApp = ViteSSG(App, { routes });
+export const createApp = ViteSSG(App, {
+  routes,
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition && !to.hash) return savedPosition;
+
+    if (to.hash) return { el: to.hash, top: 75, behavior: 'smooth' };
+
+    return { top: 0, behavior: 'smooth' };
+  },
+});
