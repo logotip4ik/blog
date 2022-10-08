7ab22a4d9204024b7ae4bb2584e1bf972e0305ba
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-10-08 23:13:41 +0300

    refactor: github link

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,11 @@ useHead({
 
 <template>
   <div index-page>
-    <h1>Blog</h1>
+    <header>
+      <h1>Blog</h1>
+
+      <a href="https://github.com/logotip4ik" target="_blank" rel="noopener noreferrer">gh</a>
+    </header>
 
     <p>This is a place to put some of my thoughts, share code snippets and improve my skills as developer. Enjoy 😁</p>
 
@@ -34,6 +38,12 @@ div[index-page] {
 
   li > span {
     float: right;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 }
 </style>
