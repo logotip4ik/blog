d83b032667e689e51492767a51f98a025819d220
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-10-08 21:59:16 +0300

    refactor: some minor changes to styles and content

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,13 +9,31 @@ useHead({
 </script>
 
 <template>
-  <div>
+  <div index-page>
+    <h1>Blog</h1>
+
+    <p>This is a place to put some of my thoughts, share code snippets and improve my skills as developer. Enjoy 😁</p>
+
     <ul>
       <li v-for="page in posts" :key="page.path">
         <RouterLink :to="page.path">
-          {{ page.name }}
+          {{ filenameToRouteName(page.filename) }}
         </RouterLink>
+
+        <span>&nbsp;{{ formatDate(page.createAt) }}</span>
       </li>
     </ul>
   </div>
 </template>
+
+<style lang="scss">
+div[index-page] {
+  h1 + p {
+    margin-bottom: 5rem;
+  }
+
+  li > span {
+    float: right;
+  }
+}
+</style>
