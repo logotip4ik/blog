d6e25f59cc515935640caa6088be36a8ee194508
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-10-27 23:37:39 +0300

    refactor: rework use raf composable

--- src/composables/use-raf.js
+++ src/composables/use-raf.js
@@ -1,6 +1,14 @@
 import { onMounted } from 'vue';
 
-const callbacks = new Map();
+/**
+ * @typedef Callback
+ * @type {object}
+ * @property {number} id
+ * @property {function} value
+ */
+
+/** @type {Callback[]} */
+const callbacks = [];
 
 let animationFrame;
 
@@ -8,19 +16,44 @@ export default function () {
   if (!animationFrame) onMounted(createAnimationFrame);
 
   return {
-    add: (value) => callbacks.set(value, value),
-    remove: (value) => callbacks.delete(value),
+    add: (value) => {
+      /** @type {Callback} */
+      const callback = { id: getId(), value };
+
+      callbacks.push(callback);
+
+      return callback.id;
+    },
+
+    remove: (id) => {
+      const index = callbacks.findIndex((callback) => callback.id === id);
+
+      callbacks.splice(index, 1);
+    },
   };
 }
 
-function createAnimationFrame() {
-  const loop = (time) => {
-    callbacks.forEach((value) => (typeof value === 'function' ? value(time) : callbacks.delete(value)));
+/** @return {number} */
+function getId() {
+  if (typeof window !== 'undefined') return Math.random();
 
-    requestAnimationFrame(loop);
+  return performance.now();
+}
+
+function createAnimationFrame() {
+  const raf = window.requestAnimationFrame;
+
+  const loop = (time) => {
+    raf(loop);
+
+    callbacks.forEach((callback) => {
+      if (typeof callback.value !== 'function') return;
+
+      callback.value(time);
+    });
   };
 
-  animationFrame = requestAnimationFrame(loop);
+  animationFrame = raf(loop);
 
   window.addEventListener('beforeunload', () => cancelAnimationFrame(animationFrame));
 }
