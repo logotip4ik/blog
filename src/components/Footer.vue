2f3755060271e0f01e0e49d498094941042ffe41
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2022-12-20 20:44:09 +0200

    refactor: remove prettier comment

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,6 @@
 <template>
   <footer>
     <small>
-      <!-- prettier-ignore -->
       <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-SA 4.0</a>
       2022-PRESENT © Bogdan Kostyuk
     </small>
