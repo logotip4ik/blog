6dd59d266034e0d3948f552f6ca3035ce9519312
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-11-20 12:31:33 +0200

    chore: migrate to eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import { antfu } from '@antfu/eslint-config';
+
+export default antfu({
+  stylistic: {
+    quotes: 'single',
+    semi: true,
+  },
+
+  rules: {
+    'arrow-parens': ['error', 'always'],
+    'curly': ['error', 'multi-or-nest', 'consistent'],
+    'antfu/if-newline': 'off',
+  },
+});
